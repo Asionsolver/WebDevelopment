5631d7257b8317fcac0e873ac411fabe065d5621
Asionsolver <asionsolver274@gmail.com>
2021-10-29 12:37:54 +0600

    Add JS File

---
+++ Javascript/OctalandHexadecimal.js
@@ -0,0 +1,7 @@
+var hex = 0xd454
+var hex1 = 0Xd454
+var oct = 0452
+
+console.log(hex)
+console.log(hex1)
+
---
+++ Javascript/Operators.js
@@ -0,0 +1,46 @@
+// Arithmetic Operator
+//+, -, *, /, %, ++-->Incremental Operator, -- -->Decremental Operator
+
+
+var a = 10
+var b = 3
+
+console.log(a / b)
+console.log(a % b)
+
+//++a-->Pre Incremental
+console.log('Pre Incremental')
+console.log(a)
+console.log(++a)
+console.log(++a)
+
+//a++-->Post Incremental
+console.log('Post Incremental')
+console.log(a)
+console.log(a++)
+console.log(a++)
+console.log(a++)
+console.log(a)
+console.log(a)
+
+//assignment operator
+//=, +=,-=,*=,/=,%=
+
+// Comparison Operator return Boolean Value
+// ==,>=,<=,!=,===,!==
+
+var str = '50'
+var n = 50
+
+console.log(str == n) //Return True Because Implicit Conversion
+console.log(str === n) //Return False
+
+// Logical Operator
+// &&--> And Operator, || --> Or Operator, !-->Not Operator
+
+// Bitwise Operator
+//&--> And Operator, |--> Or Operator,^-->Xor Operator, ~-->Not Operator,<<-->Left Shift Operator, >> -->Right Shift Operator
+
+//Type Of Operator
+console.log(typeof 10)
+console.log(typeof 'gfd')
