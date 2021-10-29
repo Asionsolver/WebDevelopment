4757e991bc6643fb5dada3cdc1eab5ab5238091a
Asionsolver <asionsolver274@gmail.com>
2021-10-29 09:16:25 +0600

    Add some html & js file

---
+++ Jacascript/Variable.js
@@ -0,0 +1,10 @@
+var name = 'Ashis kumar Pal';
+var age = 45;
+
+console.log(name)
+console.log(age)
+console.log(name + ' ' + age)
+console.log(name + ' known Javascript.');
+console.log(' is only ' + age + '.');
+console.log(name + ' is creating a Javascript course for all.');
+console.log('But his age is only ' + age + '.');
---
+++ Jacascript/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(name + ' known Javascript.');
+console.log(' is only ' + age + '.');
+console.log(name + ' is creating a Javascript course for all.');
+console.log('But his age is only ' + age + '.');
