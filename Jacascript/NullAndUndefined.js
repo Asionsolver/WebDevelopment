79ed8972b1c90e91882e9121dcb6eb2905e4ccde
Asionsolver <asionsolver274@gmail.com>
2021-10-29 10:09:13 +0600

    Add Three js file

---
+++ Jacascript/NullAndUndefined.js
@@ -0,0 +1,9 @@
+// A Variable without value always store undefined
+var abc
+
+// A variable with unknown value can store null
+// Null is a special type used as a Absence of an object
+var x = null
+
+console.log(abc)
+console.log(x)
---
+++ Jacascript/String.js
@@ -0,0 +1,16 @@
+// String Literal
+var s2 = 'String Literal'
+var s1 = "String Literal"
+var s3 = `String Literal`
+
+// String Constructor
+var sc1 = String('String Constructor 1')
+var sc2 = String('655534')
+var sc3 = String('4665.55465465')
+
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(sc1)
+console.log(sc2)
+console.log(sc3)
