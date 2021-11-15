8d97f8cf9326beaebfb15b7941748c46a0ed7343
Asionsolver <asionsolver274@gmail.com>
2021-11-15 19:25:49 +0600

    Add ObjectMethod file

---
+++ Javascript/ObjectMethod.js
@@ -0,0 +1,12 @@
+var obj = { x: 40, y: 60, z: 75 }
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+
+var obj2 = Object.assign({}, obj)
+obj2.x = 100
+obj2.y = 100
+
+console.log(obj)
+console.log(obj2)
