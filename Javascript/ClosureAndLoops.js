2950bf5b7e8d0a262f0fa61e6ecaf50cd554ce27
Asionsolver <asionsolver274@gmail.com>
2021-11-19 11:10:40 +0600

    Add file in Javascript folder

---
+++ Javascript/ClosureAndLoops.js
@@ -0,0 +1,13 @@
+for (var i = 1; i <= 5; i++) {
+    (function(n) {
+        setTimeout(function() {
+            console.log(n)
+        }, 1000 * n);
+    })(i)
+}
+
+// for (let i = 1; i <= 5; i++) {
+//     setTimeout(function() {
+//         console.log(i);
+//     }, 1000 * i);
+// }
