95d74ba4fa145f08a497fe4f87324e1d3f110133
Asionsolver <asionsolver274@gmail.com>
2022-03-11 00:10:27 +0600

    add app.js file

---
+++ Javascript/PrototypeandPrototypicalInheritance.js
@@ -0,0 +1,8 @@
+
+var obj = {}
+var obj2 = {}
+
+Object.getPrototypeOf(obj) === Object.getPrototypeOf(obj2)  //open browser console and see out put details
+
+
+
---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+let title = document.getElementById('title')  //select by id
+console.log(title)
+
+let paragraph =document.getElementsByClassName('lead') //select by class
+console.log(paragraph)
+
+let list = document.getElementsByTagName('li')
+console.log(list)
+
+let listItemOne = document.getElementsByName('list-item-one')
+console.log(listItemOne)
