aa7d356f8cad3f045bae69706bc25f450913ba43
Asionsolver <asionsolver274@gmail.com>
2022-03-11 08:47:08 +0600

    modified app.js file

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,52 @@
-let title = document.getElementById('title')  //select by id
-console.log(title)
+// let title = document.getElementById('title')  //select by id
+// console.log(title)
 
-let paragraph =document.getElementsByClassName('lead') //select by class
-console.log(paragraph)
+// let paragraph =document.getElementsByClassName('lead') //select by class
+// console.log(paragraph)
 
-let list = document.getElementsByTagName('li')
-console.log(list)
+// let list = document.getElementsByTagName('li')
+// console.log(list)
 
-let listItemOne = document.getElementsByName('list-item-one')
-console.log(listItemOne)
+// let listItemOne = document.getElementsByName('list-item-one')
+// console.log(listItemOne)
+
+// let title = document.querySelector('#title')
+// console.log(title)
+
+// let paragraph = document.querySelector('.lead')
+// console.log(paragraph)
+
+// let paragraphs = document.querySelectorAll('.lead')
+// console.log(paragraphs)
+
+// let list =document.querySelectorAll('li')
+// console.log(list)
+
+// let listItemOne =document.querySelectorAll('[name^="list-item-one"]')
+// console.log(listItemOne)
+
+let list = document.getElementById('list')
+
+// let parent = list.parentElement  // show parent element
+// console.log(parent)
+
+// let children = list.children
+// console.log(children)
+
+// let previousChildren = list.previousSibling //return node element
+// console.log(previousChildren)
+
+// let previousChildren2 = list.previousElementSibling //return html element
+// console.log(previousChildren2)
+
+// console.log(list.nextElementSibling)
+
+// let li = document.querySelector('li')
+
+// console.log(li.nextElementSibling)
+
+console.log(list.firstChild) //return first child node that means text node
+
+console.log(list.firstElementChild) // return first element child node that means first html element
+console.log(list.lastElementChild) // return first element child node that means first html element
+
