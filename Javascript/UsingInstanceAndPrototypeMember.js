472eba88d1f4db6d03acc39deee8121426c08ef8
Asionsolver <asionsolver274@gmail.com>
2022-04-08 21:10:21 +0600

    Modified Using Instance and Prototype Members.js file

--- Javascript/UsingInstanceAndPrototypeMember.js
+++ Javascript/UsingInstanceAndPrototypeMember.js
@@ -22,3 +22,11 @@ square.prototype={
 var sqr1 = new square(10);
 var sqr2 = new square(5);  //this will create two objects/instance.
 
+//hasOwnProperty() -->The hasOwnProperty() method returns a boolean indicating whether the object has the specified property as its own property (as opposed to inheriting it). The hasOwnProperty() method is a good way to check if an object has a property, without having to rely on the instanceof operator.
+
+// console.log(Object.keys(sqr1));
+
+for (var i in sqr1){
+    console.log(i);
+}
+
