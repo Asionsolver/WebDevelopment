5ef47410b676a4797b3669ee81c1ed61a9e52bad
Asionsolver <asionsolver274@gmail.com>
2022-06-04 20:16:45 +0600

    Modified CallBackFunction.js file in Javascript Folder

--- Javascript/FunctionComposition.js
+++ Javascript/FunctionComposition.js
@@ -1,14 +1,14 @@
-function print(input) {
-    console.log(input)
+function print(input) { // input = 40
+    console.log(input) // 40
 }
 
-function multiplyByFive(n) {
-    return n * 5
+function multiplyByFive(n) {  //n = 8
+    return n * 5 // n * 5 = 8 * 5 = 40
 }
 
 function add(a, b) {
 
-    return a + b
+    return a + b // a + b = 3 + 5 = 8
 }
 
 print(multiplyByFive(add(3, 5)))
