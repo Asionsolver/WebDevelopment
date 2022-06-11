cf7c779bcc8cbb9262ffc424740ca1a0406ca90f
Asionsolver <asionsolver274@gmail.com>
2022-06-11 23:50:50 +0600

    Modified ForInLoop.js file in Javascript Folder

--- Javascript/tempCodeRunnerFile.js
+++ Javascript/tempCodeRunnerFile.js
@@ -1,1 +1,10 @@
-y
+var person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+
+// for (var key in person) {
+//     console.log(key + ": " + person[key]);
+//     // console.log(key);
+// }
