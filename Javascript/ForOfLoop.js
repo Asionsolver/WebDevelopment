7cf588b5842858c92950e743ae385930c39ab024
Asionsolver <asionsolver274@gmail.com>
2022-06-12 09:47:45 +0600

    add FalsiMethod.cpp in Numerical Folder

---
+++ Javascript/ForOfLoop.js
@@ -0,0 +1,18 @@
+// ! The For Of Loop
+// The JavaScript for of statement loops through the values of an iterable object.
+
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+
+
+
+var name = "John";
+
+var numbers = [15, 285, 38, 44, 745];
+for (let key of name) {
+    console.log(key);
+}
+
+for (let key of numbers) {
+    console.log(key);
+}
+    
