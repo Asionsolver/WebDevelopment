f46b508edbabf123ec72431e4eac73e4bff720a7
Asionsolver <asionsolver274@gmail.com>
2022-06-22 20:32:34 +0600

    modified stringmethod.js file

--- Javascript/StringMethods.js
+++ Javascript/StringMethods.js
@@ -1,7 +1,7 @@
-var a = 'I am '
-var b = 'Asion'
+// var a = 'I am '
+// var b = 'Asion'
 
-var c = a.concat(b)
+// var c = a.concat(b)
     // var d = c.substring(5)
     // var e = c.substring(5, 3) //return m
     // var f = c.substr(5, 3) //return Asi
@@ -17,4 +17,33 @@ var c = a.concat(b)
 // console.log(a.toUpperCase())
 // console.log(a.toLowerCase())
 // console.log('      Asion          '.trim())
-console.log(c.split(' '))
+// console.log(c.split(' '))
+
+const a = {
+    name:"Bangladesh",
+    city:"Dhaka",
+}
+
+const s = "Belgium is not bad country."
+// console.log(s.length)
+
+/*
+
+Extracting String Parts
+There are 3 methods for extracting a part of a string:
+
+1. slice(start, end)
+2. substring(start, end)
+3. substr(start, length)
+
+*/
+
+
+// ? Slice Method
+
+/*
+slice() extracts a part of a string and returns the extracted part in a new string.
+
+The method takes 2 parameters: the start position, and the end position (end not included).
+*/
+console.log(s.slice(5,10));
