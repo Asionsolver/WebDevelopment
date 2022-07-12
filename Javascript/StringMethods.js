87fad8f5fbb0267ac62a07c177910c3cf83f9db1
Asionsolver <asionsolver274@gmail.com>
2022-07-12 11:35:05 +0600

    add someMehtod like SbuStr() in StringMethod.js file

--- Javascript/StringMethods.js
+++ Javascript/StringMethods.js
@@ -19,12 +19,12 @@
 // console.log('      Asion          '.trim())
 // console.log(c.split(' '))
 
-const a = {
-    name:"Bangladesh",
-    city:"Dhaka",
-}
+// const a = {
+//     name:"Bangladesh",
+//     city:"Dhaka",
+// }
 
-const s = "Belgium is not bad country."
+// const s = "Belgium is not bad country."
 // console.log(s.length)
 
 /*
@@ -46,5 +46,58 @@ slice() extracts a part of a string and returns the extracted part in a new stri
 
 The method takes 2 parameters: the start position, and the end position (end not included).
 */
-console.log(s.slice(5,10));
-console.log(s.slice(5));
+// console.log(s.slice(5,10));
+// console.log(s.slice(5));
+
+/*
+!Note
+* JavaScript counts positions from zero.
+
+* First position is 0.
+
+* Second position is 1.
+
+? If a parameter is negative, the position is counted from the end of the string.
+
+**This example slices out a portion of a string from position -12 to position -6:
+
+*/
+let str = "Apple, Banana, Kiwi";
+// let part = str.slice(-12, -6);
+// console.log(part);
+
+// let part_2 = str.slice(2, -6);
+// console.log(part_2);
+
+// let part_3 = str.slice(-5, -12); //return nothing
+// console.log(part_3);
+
+// ! If you omit the second parameter, the method will slice out the rest of the string
+
+// let part2 = str.slice(7);
+// console.log(part2);
+
+// ! JavaScript String substring()
+//substring() is similar to slice().
+
+// ? The difference is that start and end values less than 0 are treated as 0 in substring(). That means that Substring() can not accept negative indexes.
+
+
+// let part_s1 = str.substring(7, 13);
+// console.log(part_s1);
+
+// If you omit the second parameter, substring() will slice out the rest of the string.
+
+// const part_s2 = str.substring(7);
+// console.log(part_s2);
+
+// ! JavaScript String substr()
+// substr() is similar to slice().
+
+// The difference is that the second parameter specifies the length of the extracted part.
+// If you omit the second parameter, substr() will slice out the rest of the string.
+
+// const part_sb1 = str.substr(7, 6);
+const part_sb1 = str.substr(7);
+console.log(part_sb1);
+
