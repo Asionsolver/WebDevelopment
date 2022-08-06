c0bc58bcaeeae57a473659a8930e66edefc0a3e0
Asionsolver <asionsolver274@gmail.com>
2022-08-06 21:54:24 +0600

    Modified Closer.js file in Javascript Folder

--- Javascript/closer.js
+++ Javascript/closer.js
@@ -1,10 +1,53 @@
-function name() {
-    var x = 45
-    return function() {
-        console.log(x) //x called closer
-    }
+// function name() {
+//     var x = 45
+//     return function() {
+//         console.log(x) //x called closer
+//     }
 
-}
+// }
 
-var abc = name()
-console.dir(abc)
+// var abc = name()
+// console.dir(abc)
+
+// Simple function
+
+// var sum = function(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(sum(2,3));
+
+// var num1 = 2;
+// var num2 = 3;
+
+// var sum = function(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.dir(sum());
+
+// var sum = function(num1, num2){
+//     return function(){
+//         return num1 + num2;
+//     }
+// }
+
+// var myFunc = sum();
+
+// console.dir(myFunc);
+
+// var num1 = 3;
+
+// var sum = function(num1, num2){
+//     var num2 = 2;
+//     return function(){
+//         return num1 + num2;
+//     }
+// }
+
+// var myFunc = sum();
+
+// console.dir(myFunc);
+
+
+
