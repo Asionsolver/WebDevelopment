f65745045629e36a4490735579094224817b3329
Asionsolver <asionsolver274@gmail.com>
2022-11-28 12:05:04 +0600

    add Asynchronous Programming.js file

---
+++ Javascript/ResetConstructor.js
@@ -0,0 +1,42 @@
+function shape(){
+
+}
+
+shape.prototype.common= function(){
+        console.log("I am a Method");
+    }
+
+
+function Square(width){
+    this.width = width;
+}
+
+// Square.prototype = O   bject.create(shape.prototype);
+
+Square.prototype.draw = function(){
+    console.log('I am a Draw Method');
+}
+
+// Resetting the Constructor
+// Square.prototype = {
+//     test: function(){
+//         console.log('I am a Test Method');
+//     }   
+// }
+
+Square.prototype.test = function(){
+    console.log('I am a Test Method');
+}
+
+var shape = new shape(); // shape -> Shape -> Object
+var sqr = new Square(10); // sqr -> Square -> Object
+
+// sqr -> square -> shape -> Object
+
+// function circle(){
+
+// }
+
+// circle.prototype = Object.create(shape.prototype);
+
+// var cir = new circle();
