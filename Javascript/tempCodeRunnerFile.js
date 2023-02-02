6f2da98e28e50e44b88565d79da00f55fd1177fa
Asionsolver <asionsolver274@gmail.com>
2023-02-02 19:00:40 +0600

    Revision Asyncronous Programming, Promise, Async-Await

--- Javascript/tempCodeRunnerFile.js
+++ Javascript/tempCodeRunnerFile.js
@@ -1,3 +1,24 @@
-setTimeout(function() {
-    console.log('AsynchronousBehavior.js loaded step 1');
-},2000);
+const takeOrder = (customer, callback) => {
+  console.log(`Take order for customer ${customer}`);
+  callback(customer);
+};
+
+const processOrder = (customer, callback) => {
+  console.log(`Processing order for customer ${customer}`);
+
+  setTimeout(() => {
+    console.log(`Cooking complete`);
+    console.log(`Order processed for customer ${customer}`);
+    callback(customer);
+  }, 3000);
+};
+
+const completeOrder = (customer) => {
+  console.log(`Completed order for customer ${customer}`);
+};
+
+takeOrder("Customer 1", (customer) => {
+  processOrder(customer, (customer) => {
+    completeOrder(customer);
+  });
+});
