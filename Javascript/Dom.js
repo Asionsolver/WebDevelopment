b982b92205f4a0624d16a8a62ecba36d15399f84
Asionsolver <asionsolver274@gmail.com>
2023-03-23 12:19:05 +0600

    add file name like Dom.js

---
+++ Javascript/Dom.js
@@ -0,0 +1,6 @@
+// document.getElementById("demo").innerHTML = "Hello JavaScript!";
+// // console.dir(document.getElementById("demo"));
+// console.log(document.getElementById("demo"));
+
+const x = document.forms['frm1'];
+console.dir(x);
