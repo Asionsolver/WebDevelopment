7a4e6c039ae7d4347066501198569bda5fc19241
Asionsolver <asionsolver274@gmail.com>
2023-05-05 11:28:58 +0600

    add new object example name like rect in Thiskeyword.js file

--- Javascript/ThisKeyword.js
+++ Javascript/ThisKeyword.js
@@ -5,6 +5,34 @@
 // new myFunction();
 
 
+var rect = {
+    width: 100,
+    height: 50,
+
+    draw: function () {
+        console.log('I am rectangle.')
+        this.printProperties()
+        console.log(this)
+    },
+
+    printProperties: function () {
+        console.log('My width is ' + this.width)    
+        console.log('My height is ' + this.height)
+    }
+
+}
+
+rect.draw()
+
+var another = {
+    width: 45,
+    height: 96,
+    print: rect.printProperties
+}
+console.log(another.print)
+another.print()
+
+
 
 
 
