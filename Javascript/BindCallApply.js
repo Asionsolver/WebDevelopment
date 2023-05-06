f40bd60a193c1c590442cd572804e7688dcc5bac
Asionsolver <asionsolver274@gmail.com>
2023-05-07 04:04:36 +0600

    use bind method

--- Javascript/BindCallApply.js
+++ Javascript/BindCallApply.js
@@ -13,4 +13,10 @@ function myFunction(c, d) {
 
 // myFunction()
 // myFunction.call({ a: 10, b: 20 }, 30, 40);
-myFunction.apply({ a: 15, b: 25 }, [30, 40]);
+// myFunction.apply({ a: 15, b: 25 }, [30, 40]);
+
+// myFunction.bind({ a: 7, b: 3 }, 30, 40) // this code will not work because bind return a function not call it. That means bind method bind a function with a object and return a function.
+
+// var test = myFunction.bind({ a: 7, b: 3 }, 30, 40)
+var test = myFunction.bind({ a: 7, b: 3 })
+test(30, 40) // this code will work because we call the function which is return by bind method.
