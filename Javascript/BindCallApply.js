9172089dc2d44746fa0a91152b0a8050f2369730
Asionsolver <asionsolver274@gmail.com>
2023-05-07 03:32:32 +0600

    use apply method in different example

--- Javascript/BindCallApply.js
+++ Javascript/BindCallApply.js
@@ -1,4 +1,4 @@
-// function myFunction(a, b) {
+// function myFunction() {
      // console.log(this)
 //     console.log(this.a + this.b)
 // }
@@ -6,3 +6,11 @@
  // myFunction()
 // myFunction.call({ a: 10, b: 20 });
 
+function myFunction(c, d) {
+    // console.log(this)
+    console.log(this.a + this.b + c + d)
+}
+
+// myFunction()
+// myFunction.call({ a: 10, b: 20 }, 30, 40);
+myFunction.apply({ a: 15, b: 25 }, [30, 40]);
