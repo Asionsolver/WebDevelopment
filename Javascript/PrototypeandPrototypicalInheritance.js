2d9a725a390c4e2ba4a5d4833087ad0bf42cfa26
Asionsolver <asionsolver274@gmail.com>
2023-05-07 11:32:10 +0600

    modified Prototype and Prototypical Inheritance.js file

--- Javascript/PrototypeandPrototypicalInheritance.js
+++ Javascript/PrototypeandPrototypicalInheritance.js
@@ -2,6 +2,8 @@
 var obj = {}
 var obj2 = {}
 
+obj.__proto__ === obj2.__proto__ //true
+
 var result = Object.getPrototypeOf(obj) === Object.getPrototypeOf(obj2)  //open browser console and see out put details
 
 console.log(result)
