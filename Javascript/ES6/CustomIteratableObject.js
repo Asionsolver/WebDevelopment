6ad9c05bfae6f148941f8b01dc43d6aa3fd62d20
Asionsolver <asionsolver274@gmail.com>
2023-05-13 17:34:56 +0600

    add new file name like CustomIterableObject.js file

---
+++ Javascript/ES6/CustomIteratableObject.js
@@ -0,0 +1,20 @@
+let obj ={
+    start: 1,
+    end: 10,
+    [Symbol.iterator]: function(){
+        let currentValue = this.start;
+        const self = this;
+        return {
+            next(){
+                return {
+                    done: currentValue > self.end,
+                    value: currentValue > self.end ? undefined : currentValue++
+                }
+            }
+        }
+    }
+}
+
+for(let i of obj){
+    console.log(i);
+}
