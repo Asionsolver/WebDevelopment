66dad9ab63a26da32ff0abf03d1ba7b819afc727
Asionsolver <asionsolver274@gmail.com>
2023-05-13 23:56:28 +0600

    add new file name like ObjectIterables.js file

---
+++ Javascript/ES6/ObjectIterables.js
@@ -0,0 +1,33 @@
+const arr = [1, 2, 3, 4, 5];
+
+const obj = {
+    first: 1,
+    second: 2,
+    third: 3,
+    fourth: 4,
+}
+
+// console.dir(arr);
+// console.dir(obj);
+
+
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let i of arr) {
+//     console.log(i);
+// }
+
+
+for (let i in obj) {
+    console.log(i);
+}
+
+// object iterate in for loop
+
+// for(let i= 0; i < Object.keys(obj).length; i++) {
+//     console.log(obj[Object.keys(obj)[i]]);
+// }
+    
