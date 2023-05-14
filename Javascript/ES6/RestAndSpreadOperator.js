432d224e56a9e5d08cf68e69a47911cc508f468d
Asionsolver <asionsolver274@gmail.com>
2023-05-14 22:17:28 +0600

    add new method name like sum using Rest operator

--- Javascript/ES6/RestAndSpreadOperator.js
+++ Javascript/ES6/RestAndSpreadOperator.js
@@ -1,3 +1,5 @@
+// calculate sum of all arguments passed to a function in normal way
+
 function sum() {
     let sum = 0;
     for (let i = 0; i < arguments.length; i++) {
@@ -5,6 +7,15 @@ function sum() {
     }
     return sum;
     
+}
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+
+// calculate sum of all arguments passed to a function in Rest operator way
+
+function sum(...rest){
+    return rest.reduce((a, b) => a + b);
 }
 
 console.log(sum(1, 2, 3, 4, 5));
