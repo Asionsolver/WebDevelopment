418d6811f83402d2ff4d594730bd6a1a0536a716
Asionsolver <asionsolver274@gmail.com>
2023-05-14 22:31:12 +0600

    add new method name like myBio using rest operator

--- Javascript/ES6/RestAndSpreadOperator.js
+++ Javascript/ES6/RestAndSpreadOperator.js
@@ -1,21 +1,32 @@
 // calculate sum of all arguments passed to a function in normal way
 
 function sum() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-    
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
 }
 
 // console.log(sum(1, 2, 3, 4, 5));
 
-
 // calculate sum of all arguments passed to a function in Rest operator way
 
-function sum(...rest){
-    return rest.reduce((a, b) => a + b);
+function sum(...rest) {
+  return rest.reduce((a, b) => a + b);
 }
 
 console.log(sum(1, 2, 3, 4, 5));
+
+
+
+// Use rest to enclose the rest of specific user-supplied values into an array:
+function myBio(firstName, lastName, ...otherInfo) { 
+    return otherInfo;
+  }
+  
+  // Invoke myBio function while passing five arguments to its parameters:
+  myBio("Oluwatobi", "Sofela", "CodeSweetly", "Web Developer", "Male");
+  
+  // The invocation above will return:
+  ["CodeSweetly", "Web Developer", "Male"]
