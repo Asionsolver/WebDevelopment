c501d44aa1059ce48bfc06f8d048cf15c7358777
Asionsolver <asionsolver274@gmail.com>
2023-05-15 20:10:24 +0600

    Add new topic name like spread operator

--- Javascript/ES6/RestAndSpreadOperator.js
+++ Javascript/ES6/RestAndSpreadOperator.js
@@ -18,15 +18,36 @@ function sum(...rest) {
 
 console.log(sum(1, 2, 3, 4, 5));
 
-
-
 // Use rest to enclose the rest of specific user-supplied values into an array:
-function myBio(firstName, lastName, ...otherInfo) { 
-    return otherInfo;
-  }
-  
-  // Invoke myBio function while passing five arguments to its parameters:
-  myBio("Oluwatobi", "Sofela", "CodeSweetly", "Web Developer", "Male");
-  
-  // The invocation above will return:
-  ["CodeSweetly", "Web Developer", "Male"]
+function myBio(firstName, lastName, ...otherInfo) {
+  return otherInfo;
+}
+
+// Invoke myBio function while passing five arguments to its parameters:
+myBio("Oluwatobi", "Sofela", "CodeSweetly", "Web Developer", "Male");
+
+// The invocation above will return:
+["CodeSweetly", "Web Developer", "Male"];
+
+
+let arr1 = [1, 2, 3, 4, 5];
+
+console.log(...arr1);
+
+let obj1 = {
+    name: "Tobi",
+    age: 20,
+    class: "SS3"
+}
+
+let obj2 = {
+    ...obj1,
+}
+
+console.log(obj2);
+
+console.log(obj1 === obj2);
+
+let obj3 = obj1;
+
+console.log(obj3 === obj1);
