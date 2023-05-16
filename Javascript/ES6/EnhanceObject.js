b84918536c94f77ed2ef21778f1abcd1eb3b29b3
Asionsolver <asionsolver274@gmail.com>
2023-05-16 22:21:48 +0600

    modified EnhanceObject.js file add new topic name like object method declaration shorthand

--- Javascript/ES6/EnhanceObject.js
+++ Javascript/ES6/EnhanceObject.js
@@ -2,17 +2,24 @@ let a=10, b= 20;
 
 
 // ES5 Object Literal Syntax (Shorthand Property)
-// let obj = {
-//     a: a,
-//     b: b,
-// }
-
-// console.log(obj); // {a: 10, b: 20}
-
-// ES6 Enhance Object Literal Syntax (Shorthand Property)
 let obj = {
-    a,
-    b,
+    a: a,
+    b: b,
+    print: function() { 
+        console.log(this);
+    }
 }
 
 console.log(obj); // {a: 10, b: 20}
+obj.print();
+
+// ES6 Enhance Object Literal Syntax (Shorthand Property)
+// let obj = {
+//     a,
+//     b,
+//     print() {
+//         console.log(this);
+//     }
+// }
+
+// console.log(obj); // {a: 10, b: 20}
