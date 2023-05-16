5eba580e422c82c80672eb517ed9a9800bc5247f
Asionsolver <asionsolver274@gmail.com>
2023-05-16 22:17:04 +0600

    add new file name like EnhanceObject.js file

---
+++ Javascript/ES6/EnhanceObject.js
@@ -0,0 +1,18 @@
+let a=10, b= 20;
+
+
+// ES5 Object Literal Syntax (Shorthand Property)
+// let obj = {
+//     a: a,
+//     b: b,
+// }
+
+// console.log(obj); // {a: 10, b: 20}
+
+// ES6 Enhance Object Literal Syntax (Shorthand Property)
+let obj = {
+    a,
+    b,
+}
+
+console.log(obj); // {a: 10, b: 20}
