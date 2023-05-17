b2d11cfc633ac6ea0ccdb7ae20d0da9e730799c5
Asionsolver <asionsolver274@gmail.com>
2023-05-17 20:00:48 +0600

    modified symbol.js file and learn new topic name like use case of symbol data type

--- Javascript/ES6/symbols.js
+++ Javascript/ES6/symbols.js
@@ -12,6 +12,20 @@ console.log(s1 === s2); // false
 
 console.log(a === b); // true --> This is the main difference between Symbol and other primitive types.
 
+let tossingCoin = {
+    HEAD: Symbol("HEAD"),
+    TAIL: Symbol("TAIL")
+}
+
+console.log(tossingCoin.HEAD); // Symbol(HEAD)
+console.log(tossingCoin.TAIL); // Symbol(TAIL)
+
+console.log(tossingCoin.HEAD === tossingCoin.TAIL); // false
+
+console.log(tossingCoin.HEAD === tossingCoin.HEAD); // true
+
+console.log(tossingCoin.TAIL === tossingCoin.TAIL); // true
+
 
 
 
