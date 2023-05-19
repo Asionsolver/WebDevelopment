4dc5b92e80d09fc72a2bc89ad898432bdc25c122
Asionsolver <asionsolver274@gmail.com>
2023-05-19 10:32:09 +0600

    add new file name like forOfLoop.js file

---
+++ Javascript/ES6/forOfLoop.js
@@ -0,0 +1,21 @@
+let arr = [1,2,3,4,5]
+
+for (let value of arr) {
+    console.log(value);
+}
+
+let str ='Ashis';
+
+for(let value of str){
+    console.log(value)
+}
+
+const obj ={
+    a:20,
+    b:20,
+    c:30
+}
+
+for(let value of obj){
+    console.log(value)
+}
