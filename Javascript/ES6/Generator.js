a08642104fdf24223297029f49f843f43b51446f
Asionsolver <asionsolver274@gmail.com>
2023-05-20 21:54:30 +0600

    modified Generator.js file and implement our custom generator function

--- Javascript/ES6/Generator.js
+++ Javascript/ES6/Generator.js
@@ -8,3 +8,47 @@ const obj ={
         }
     }
 }
+
+// let iterate = obj[Symbol.iterator]()
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+// Implementing a generator function
+
+// function* generator(){
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     yield 4;
+//     yield 5;
+// }
+
+// let iterate = generator() // returns an iterator
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+let arr = [1,2,3,4,5]
+
+function* generator(collection){
+    for(let i=0;i<collection.length;i++){
+        yield collection[i]
+    }
+}
+
+let iterate = generator(arr) // returns an iterator
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+
+
