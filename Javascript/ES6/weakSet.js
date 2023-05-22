8fe1ff18a171ee9784cd2a3db41d61832fe2d853
Asionsolver <asionsolver274@gmail.com>
2023-05-23 02:02:32 +0600

    modified weakSet.js file and learn some new method of weakSet

--- Javascript/ES6/weakSet.js
+++ Javascript/ES6/weakSet.js
@@ -2,3 +2,21 @@
 * @WeakSet 
 */
 
+let a = {a:1}, b = {b:2};
+
+// let set = new Set([a,b]);
+
+// a = null;  // a is not referenced anymore. so it will be garbage collected. Not cleared from set value of a.
+
+// console.log(set)
+
+// let arr = [...set];
+// console.log(arr)
+// console.log(arr[0])
+
+let weakSet = new WeakSet([a,b]);
+
+a = null;
+
+console.log(weakSet.has(a)) 
+console.log(weakSet.has(b)) 
