07ca7789c017bc7580382c4f1bdf332e2074a09d
Asionsolver <asionsolver274@gmail.com>
2023-05-23 02:11:45 +0600

    modified weakMap.js file and learn new method of weakMap

--- Javascript/ES6/weakMap.js
+++ Javascript/ES6/weakMap.js
@@ -4,3 +4,19 @@
 
 let a = {a:1}, b = {b:2};
 
+// let map = new Map([[a, 'a'], [b, 'b']]);
+
+// a = null;
+
+// console.log(map)
+
+let weakMap = new WeakMap([[a, 45], [b, 56]]);
+
+a = null;
+
+console.log(weakMap.get(1))
+console.log(weakMap.has(a))
+
+
+console.log(weakMap.get(b))
+console.log(weakMap.has(b))
