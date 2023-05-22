b069775d09c23dd356e4fd1d1ba56d497b926821
Asionsolver <asionsolver274@gmail.com>
2023-05-22 20:44:21 +0600

    add new file name like map.js

---
+++ Javascript/ES6/map.js
@@ -0,0 +1,40 @@
+/**
+* @Map is a new data structure introduced in ES6.
+*/
+
+// Map is a collection of keyed data items, just like an Object. But the main difference is that Map allows keys of any type.
+
+let map = new Map([
+    ['a', 1],
+    ['b', 2],
+]);
+
+console.log(map.get('a')); // 1
+
+map.set('c', 3);
+
+console.log(map.get('c')); // 3
+
+console.log(map.size); // 3
+
+console.log(map.has('a')); // true
+
+// map.delete('a');
+
+console.log(map.has('a')); // false
+
+console.log(map.values()); // MapIterator { 1, 2, 3 }   
+
+console.log(map.keys()); // MapIterator { 'a', 'b', 'c' }   
+
+console.log(map.entries()); // MapIterator { [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] }  
+
+
+
+
+
+
+
+
+// map.clear();
+
