0c4f00b538dd92a8835f2064fdbd1b33031e7207
Asionsolver <asionsolver274@gmail.com>
2023-06-21 16:30:18 +0600

    add new file name like InheritanceES6.js file

---
+++ Javascript/InheritanceES6.js
@@ -0,0 +1,40 @@
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  color() {
+    console.log(this.color);
+  }
+
+  draw() {
+    console.log("Drawing");
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(color,width, height) {
+    super(color);
+    this.width = width;
+    this.height = height;
+  }
+  calculate() {
+    return this.width * this.height;
+  }
+}
+
+class Circle extends Shape {
+  calculate() {
+    console.log("Circle...");
+  }
+}
+
+let r1 = new Rectangle('red',10, 20);
+console.log(r1);
+r1.draw();
+// r1.color();
+
+ 
+// let c1 = new Circle('Red');
+// console.log(c1);
+// c1.draw();
+// c1.color();
