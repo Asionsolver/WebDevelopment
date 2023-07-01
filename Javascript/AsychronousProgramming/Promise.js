859348181e002220da6d3b77887a4c203abd3e46
Asionsolver <asionsolver274@gmail.com>
2023-07-01 10:31:57 +0600

    add new file name like Promise.js file

---
+++ Javascript/AsychronousProgramming/Promise.js
@@ -0,0 +1,48 @@
+// let p1 = new Promise((resolve, reject)=>{
+
+// })
+
+// console.log(p1)
+
+// let p1 = new Promise((resolve, reject)=>{
+//     setTimeout(resolve, 5000, 'One')
+
+// })
+
+// let p2 = new Promise((resolve, reject)=>{
+//     setTimeout(resolve, 3000, 'Two')
+
+// })
+
+// // console.log(p1)
+// // console.log(p2)
+
+// p1.then((v)=>{
+//     console.log(v)
+// })
+
+// p2.then((v)=>{
+//     console.log(v)
+// })
+
+function getIphone(isPassed) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isPassed) {
+        resolve("I have got an IPHONE 15 PRO MAX");
+      } else {
+        reject(new Error("You have failed"));
+      }
+    }, 5000);
+  });
+}
+
+// console.log(getIphone(true));
+
+getIphone(true)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
