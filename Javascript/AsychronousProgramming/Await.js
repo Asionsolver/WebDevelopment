385a2f2ab5c201fa3f98bbe5bc2c42676a4b6860
Asionsolver <asionsolver274@gmail.com>
2023-07-05 23:46:10 +0600

    git add new file name like Await.js file

---
+++ Javascript/AsychronousProgramming/Await.js
@@ -0,0 +1,11 @@
+// function myPromise(){
+//     return Promise.resolve('Hello World');
+// }
+
+// console.log(myPromise());
+
+async function myPromise(){
+    // return 'Hello World';
+}
+
+console.log(myPromise());   
