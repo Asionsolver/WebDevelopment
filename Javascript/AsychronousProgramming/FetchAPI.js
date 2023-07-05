75e69ace5292b00b10c2b0e1d0daa70aae3f78bd
Asionsolver <asionsolver274@gmail.com>
2023-07-05 12:31:38 +0600

    add new file name like FetchAPI.js file

--- Javascript/AsychronousProgramming/FetchAPI.js
+++ Javascript/AsychronousProgramming/FetchAPI.js
@@ -1,18 +1,52 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
-fetch(`${BASE_URL}/users/1`)
-    .then(response=>response.json())
+// fetch(`${BASE_URL}/users/1`)
+//     .then(response=>response.json())
+//     .then(data=>{
+//         console.log(data)
+//         return Promise.resolve('Somethings')
+//     })
+//     .then(str =>{
+//         console.log(str);
+//         return Promise.resolve('Another Process')
+//     })
+//     .then(another=>{
+//         console.log(another)
+//     })
+//     .catch(error=>{
+//         console.log(error)
+//     })
+
+function getRequest(url){
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET',url);
+        
+        xhr.onreadystatechange = function(e){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    let response = JSON.parse(xhr.response);
+                    resolve(response);
+                }else{
+                    reject(new Error('Error Occured'));
+                }
+            }
+        }
+        xhr.send();
+        
+    })
+}
+
+
+// let res =  getRequest(`${BASE_URL}/users/1`)
+
+// console.log(res)
+
+
+ getRequest(`${BASE_URL}/users/1`)
     .then(data=>{
         console.log(data)
-        return Promise.resolve('Somethings')
-    })
-    .then(str =>{
-        console.log(str);
-        return Promise.resolve('Another Process')
-    })
-    .then(another=>{
-        console.log(another)
     })
     .catch(error=>{
-        console.log(error)
-    })
+        console.log(error)  
+    });
